d11242308c1e6b0bbcb270b7d1656b26793341ac
Biswa Baibhab Subudhi <94667039+biswabaibhab007@users.noreply.github.com>
2023-07-26 17:37:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const greet = document.querySelector('.greet');
+const name = document.querySelector('.name');
+const intText = document.querySelector('.interest-text');
+
+const arr = ["Web Development", "Web Designing", "Open Source"];
+
+document.addEventListener("DOMContentLoaded",()=>{
+    greet.classList.add("text-visible");
+    name.classList.add("text-visible");
+    intText.classList.add("text-visible");
+
+});
+
+window.setInterval(() => {
+    intText.innerHTML = `<p>I'm into <span class="accent-blue">${arr[Math.floor(Math.random() * arr.length)]}</span></p>`;
+}, 1000);
